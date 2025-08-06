cf5466423211af085e2a8cfff5ee662cf4b818ce
AntonioCar0lin0 <antoniorpcarolino@gmail.com>
2025-08-06 09:02:12 -0300

    fix: cors allow

--- main.py
+++ main.py
@@ -32,11 +32,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # --- Pydantic Model para a entrada da API ---
 class EntradaTeste(BaseModel):
